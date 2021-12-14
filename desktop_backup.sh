ce710049241f5e668fba732dea9aff886e4022e2
Ciaran Maye <ciaranmaye8@gmail.com>
2021-12-14 17:42:10 +0000

    .

--- desktop_backup.sh
+++ desktop_backup.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
+echo "-------------------------------------"
+echo "--          Desktop Setup          --"
+echo "-------------------------------------"
+sleep 2s
 #####
 # Install Orchis Theme
 #####
+
+echo "Installing Orchis Theme"
 git clone git@github.com:vinceliuice/Orchis-theme.git
 
 cd Orchis-theme
 sudo chmod +x install.sh
 ./install.sh
 
+echo "Orchis Theme Installed"
+sleep 2s
 
 #####
 # Install Tela Icons
 #####
+echo
 git clone git@github.com:vinceliuice/Tela-icon-theme.git
 cd Tela-icon-theme
 
@@ -26,5 +35,5 @@ sudo chmod +x install.sh
 dconf load /org/cinnamon/ < cinnamon_desktop_backup
 
 # Remove folders
-rm -r Orchis-theme/
-rm -r Tela-icon-theme/
+rm -r /Orchis-theme/ -y
+rm -r /Tela-icon-theme/ -y
--- post-installer.sh
+++ post-installer.sh
@@ -1,40 +1,80 @@
 #!/bin/bash
 
-######
+
+echo "-------------------------------------"
+echo "--     Monc's Automated Setup      --"
+echo "-------------------------------------"
+sleep 2s
+echo "-------------------------------------"
+echo "--          System Setup           --"
+echo "-------------------------------------"
+
+###############
 # Pre install
-######
+###############
 
-# Activate snap store
-sudo rm /etc/apt/preferences.d/nosnap.pref
+### Activate snap store
+sudo rm /etc/apt/preferences.d/nosnap.pref &> /dev/null
 
-# Add google chrome repo
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#### Add google chrome repo
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - &> /dev/null
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
-# Update sources
+#### Update sources
 sudo apt update
 
+echo "Sources Updated"
+sleep 1s
+
+echo "Installing Snap Store and Flatpak"
 sudo apt install snapd flatpak -y
 
-#######
+echo "Snap Store and Flatpak Installed"
+echo "Commencing install of Packages"
+sleep 2s
+
+
+###########
 # Install
-#######
+###########
 
 # Apt Install
-sudo apt install git curl wget -y
-sudo apt install gimp filezilla default-jdk google-chrome-stable thunderbird -y
+APT_PKGS="gimp filezilla default-jdk google-chrome-stable thunderbird"
+for i in $APT_PKGS; do
+  sudo apt-get install -y $i &> /dev/null
+  echo "$i installed"
+done
+
+
 
 # Snap Install
-sudo snap install teams 
-sudo snap install android-studio --classic
-sudo snap install discord --classic
+SNAP_PKGS="teams"
+CLASSIC_SNAP_PKGS="android-studio discord"
+
+for i in $SNAP_PKGS; do
+    sudo snap install $i
+    echo "$i installed"
+done
+
+for i in $CLASSIC_SNAP_PKGS; do
+    sudo snap install $i --classic
+    echo "$i installed"
+done
+
 
 # Flatpak Install
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &> /dev/null
 
+
+sleep 3s
+
+
+echo "All Packages Installed"
+sleep 1s
 ######
 # ZSH Setup
 ######
+echo "Installing zsh terminal"
 
 sudo apt install zsh
 sudo chsh -s $(which zsh)
@@ -44,11 +84,15 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 cp .zshrc ~/.zshrc
 export PATH=$HOME/bin:/usr/local/bin:$PATH
 
+echo "zsh terminal installed with all themes from 'Oh-My-Zsh!'"
+
+
+sleep 2s
 
 
 #######
 # Desktop Setup
 #######
-#FIXME:
+echo "Setting up Desktop Environment"
 sudo chmod +x .desktop_backup.sh
-./desktop_backup.sh
+sh ./desktop_backup.sh
