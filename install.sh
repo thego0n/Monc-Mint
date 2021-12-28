294cab5dec7ba229cb4e0ea3e943f30dc6e9c747
Ciaran Maye <58376132+monc949@users.noreply.github.com>
2021-12-28 19:27:09 +0000

    .

--- install.sh
+++ install.sh
@@ -100,6 +100,7 @@ sleep 1s
 echo "Installing Oh-My-bash (Terminal Themes)"
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+sleep 1s
 rm  /home/$(logname)/.bashrc
 cp  /home/$(logname)/Monc-Mint/.bashrc /home/$(logname)/.bashrc
 echo "Oh-My-Bash installed!!!"
