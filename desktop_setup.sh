1f21b9d988e2d8f8600809c5ccc893ba6cb11f23
Ciaran <ciaranmaye8@gmail.com>
2021-12-28 17:39:31 +0000

    Desktop setup changes

--- desktop_setup.sh
+++ desktop_setup.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-dconf load / < dconf_dump.txt
+dconf load /org/cinnamon/ < desktop_conf.txt
+
+echo "######################"
+echo "######################"
+echo "All finished"
+echo "Final reminder to generate a SSH key for github using 'ssh-keygen' in your home directory,"
+echo "hit enter until it finishes, and then,"
+echo "cat .ssh/id_rsa.pub,"
+echo "copy that output, go to github settings, hit 'SSH and GPG keys', make a new key and paste the output."
+echo "#######################"
+echo "You should reboot the system to finalise all changes"
