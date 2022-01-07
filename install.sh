7dcf67a84d9e0bc10faea9d2e43e17127793a2ef
Ciaran <ciaranmaye8@gmail.com>
2022-01-07 21:34:08 +0000

    restructure

--- install.sh
+++ install.sh
@@ -101,4 +101,61 @@ sudo apt update
 sudo apt upgrade
 
 
-./oh-my-bash-install.sh
+
+
+
+#####
+# Oh My Bash Install
+#####
+bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" --unattended
+
+
+
+echo "-------------------------------------"
+echo "--      Desktop/Theme Setup        --"
+echo "-------------------------------------"
+sleep 2s
+
+cd ..
+sed -i "s/font/agnoster/" .bashrc
+cd Monc-Mint/
+
+#####
+# Install Orchis Theme
+#####
+
+echo "Installing Orchis Theme"
+git clone https://github.com/vinceliuice/Orchis-theme.git
+
+cd Orchis-theme
+./install.sh
+
+cd ..
+sleep 2s
+
+#####
+# Install Tela Icons
+#####
+git clone https://github.com/vinceliuice/Tela-icon-theme.git
+cd Tela-icon-theme
+
+./install.sh
+cd ..
+
+sudo rm -r Orchis-theme/
+sudo rm -r Tela-icon-theme/
+
+######
+# Load theme and desktop settings
+######
+echo "-------------------------------------"
+echo "--        Installing Theme         --"
+echo "-------------------------------------"
+sleep 2s
+
+dconf load /org/cinnamon/ < dconf/cinnamon.dconf
+dconf load /org/nemo/ < dconf/nemo.dconf
+dconf load /org/gtk/ < dconf/gtk.dconf
+dconf load /org/gnome/ < dconf/gnome.dconf
+
+sudo ./finish.sh
--- oh-my-bash-theme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ..
-
-sed -i "s/font/agnoster/" .bashes
-
-cd Monc-Mint/
-./theme_install.sh
