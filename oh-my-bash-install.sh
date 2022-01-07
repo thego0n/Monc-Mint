7659deacb8a5d70c7ef542c5ca20a9a36714709f
Ciaran Maye <ciaranmaye8@gmail.com>
2022-01-07 19:48:24 +0000

    .

--- oh-my-bash-install.sh
+++ oh-my-bash-install.sh
@@ -3,4 +3,4 @@
 bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" --unattended
 sudo cp -f .bashrc ~/.bashrc
 
-sudo ./theme_install.sh
+./theme_install.sh
